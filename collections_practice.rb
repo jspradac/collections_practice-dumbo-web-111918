583094beb12ef9251120afb50507ac623f658e50
Samuel Prada <jspradac@gmail.com>
2018-11-21 14:07:05 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,14 @@
-
+def sort_array_asc (array)
+  array.reverse!
+end 
+def sort_array_desc(array)
+  array.sort.reverse!
+end 
+def sort_array_char_count(array)
+  array.sort_by { |string| string.size }
+end 
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  array
+end 
+def reverse_array
